f5fe96c48f842163587d07e7ab6a5c8d2595da9a
nvdovin <narmunine@gmail.com>
2023-07-14 00:26:23 +0300

    3-rd exercise Финальная версия Домашнего задания

--- catalog/models.py
+++ catalog/models.py
@@ -13,10 +13,10 @@ class Product(Model):
     title = models.CharField(max_length=20, verbose_name="Наименование")
     description = models.TextField(verbose_name="Описание")
     image = models.ImageField(upload_to="catalog/media/", **NULLABLE, verbose_name="Изображние")
-    category = models.CharField(max_length=20, verbose_name="Категория")
-    price = models.FloatField(verbose_name="Цена за товар")
-    creations_date = models.DateField(verbose_name="Дата создания")
-    last_change_date = models.DateTimeField(verbose_name="Дата последнего изменения")
+    category = models.CharField(max_length=20, verbose_name="Категория", **NULLABLE)
+    price = models.FloatField(verbose_name="Цена за товар", **NULLABLE)
+    creations_date = models.DateField(verbose_name="Дата создания", **NULLABLE)
+    last_change_date = models.DateField(**NULLABLE, verbose_name="Дата последнего изменения")
 
     def __str__(self):
         return f"{self.title} {self.category} {self.price}"
@@ -29,7 +29,6 @@ class Product(Model):
 class Category(Model):
     title = models.CharField(max_length=20, verbose_name="Наименование")
     description = models.TextField(verbose_name="Описание")
-    created_at = models.DateField(verbose_name="Creations Date", **NULLABLE)
 
     def __str__(self):
         return f"{self.title}"
